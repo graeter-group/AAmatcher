trained a model on larger peptides and fixed error in gradient ordering

AAmatcher/xyz2res/scripts/train.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
 
     model = Representation(256,out_feats=1,n_residuals=2,n_conv=1)
 
-    graphs, _ = load_graphs("./tmp/pep2.bin")
+    graphs, _ = load_graphs("./tmp/pdb_ds.bin")
 
     loss_fn = torch.nn.BCEWithLogitsLoss()
     optim = torch.optim.Adam(model.parameters(), lr=1e-5)
@@ -56,5 +56,5 @@ if __name__ == "__main__":
     # %%
     model.eval()
     model = model.to("cpu")
-    torch.save(model.state_dict(), Path(__file__).parent.parent / Path("model.pt"))
+    torch.save(model.state_dict(), Path(__file__).parent.parent / Path("model2.pt"))
     # %%

example/testing.py
@@ -11,8 +11,11 @@ import tempfile
 import os
 import matplotlib.pyplot as plt
 
-
 #%%
+"""
+For all pdb at the testpath, check wheter the workflow xyz -> PDBMolecule -> pdb -> openmm pdb runs without throwing an error
+"""
+
 def testrun(testpath=DATASET_PATH):
     fine = 0
     crashed = []
@@ -78,7 +81,124 @@ def testrun(testpath=DATASET_PATH):
 
 
 # %%
-c = testrun()
 
-# seems to crash for pro and phe
+"""
+create a dataset using openmm, then shuffle positions, elements and forces, create a mol and use openmm to calculate the energies and forces belonging to the (re-ordered) positions of the mol, and compare these elementwise. 
+"""
+
+def data_testrun(pdbpath):
+    from openmm.app import ForceField, PDBFile, topology, Simulation, PDBReporter, PME, HBonds, NoCutoff, CutoffNonPeriodic
+    from openmm import LangevinMiddleIntegrator, Vec3
+    from openmm.unit import Quantity, picosecond, kelvin, kilocalorie_per_mole, picoseconds, angstrom, nanometer, femtoseconds, newton
+
+
+    pdb = PDBFile(str(pdbpath))
+
+    residues = []
+
+    elements = []
+    for a in pdb.topology.atoms():
+        elements.append(a.element.atomic_number)
+        residues.append(a.residue.name)
+
+    residues = list(set(residues))
+    if "HIS" in residues or "GLY" in residues:
+        raise RuntimeError("forbidden residue")
+
+    elements = np.array(elements)
+
+    # generate a dataset
+
+    integrator = LangevinMiddleIntegrator(300*kelvin, 1/picosecond, 0.5*femtoseconds)
+
+    forcefield=ForceField('amber99sb.xml')
+    system = forcefield.createSystem(pdb.topology)
+
+    simulation = Simulation(
+        pdb.topology, system=system, integrator=integrator
+    )
+
+    simulation.context.setPositions(pdb.positions)
+    simulation.minimizeEnergy(maxIterations=int(10**6))
+
+    amber_e = []
+    amber_f = []
+    xyz = []
+    for t in range(20):
+        simulation.step(100)
+        state = simulation.context.getState(
+                getEnergy=True,
+                getPositions=True,
+                getForces=True,
+            )
+        e = state.getPotentialEnergy()
+        e = e.value_in_unit(kilocalorie_per_mole)
+        f = state.getForces(True)
+        f = f.value_in_unit(kilocalorie_per_mole/angstrom)
+        f = np.array(f)
+        pos = state.getPositions().value_in_unit(angstrom)
+
+        xyz.append(np.array(pos))
+        amber_e.append(e)
+        amber_f.append(f)
+
+    amber_e = np.array(amber_e)
+    amber_f = np.array(amber_f)
+    xyz = np.array(xyz)
+
+    perm = np.arange(xyz.shape[1])
+    np.random.shuffle(perm)
+    xyz = xyz[:,perm]
+    elements = elements[perm]
+    amber_f = amber_f[:,perm]
+
+    # generate a pdb molecule with our package
+    mol = PDBMolecule.from_xyz(xyz=xyz, elements=elements, energies=amber_e, gradients=-amber_f)
+    with tempfile.TemporaryDirectory() as tmp:
+        path = os.path.join(tmp, 'pep.pdb')
+        mol.write_pdb(path)
+        gen_pdb = PDBFile(path)
+
+    integrator = LangevinMiddleIntegrator(300*kelvin, 1/picosecond, 0.5*femtoseconds)
+    system = forcefield.createSystem(gen_pdb.topology)
+    simulation = Simulation(
+        gen_pdb.topology, system=system, integrator=integrator
+    )
+
+    # compare the datasets that should match
+    new_forces = []
+    new_energies = []
+    for pos in mol.xyz:
+        simulation.context.setPositions(pos/10) # go to nanometer
+        state = simulation.context.getState(
+                getEnergy=True,
+                getParameters=True,
+                getForces=True,
+            )
+        e = state.getPotentialEnergy()
+        e = e.value_in_unit(kilocalorie_per_mole)
+        f = state.getForces(True)
+        f = f.value_in_unit(kilocalorie_per_mole/angstrom)
+        f = np.array(f)
+        new_energies.append(e)
+        new_forces.append(f)
+
+    new_forces = np.array(new_forces)
+    new_energies = np.array(new_energies)
+
+    diff = new_energies - mol.energies
+    print("diff: ", np.sum(diff**2)/len(diff), "variance: ", np.sum((amber_e-np.mean(amber_e))**2)/len(diff))
+
+
+    diff2 = -new_forces - mol.gradients
+    print("diff:,", np.sum(diff2**2)/len(diff2), "~variance: ", np.sum(new_forces**2)/len(diff2))
+
+
+
+#%%
+p = Path("/hits/fast/mbm/seutelf/data/raw_pdb_files")/Path("pep2/AA/pep.pdb")
+data_testrun(p)
+    
 # %%
+c = testrun()
+# seems to crash for pro and phe